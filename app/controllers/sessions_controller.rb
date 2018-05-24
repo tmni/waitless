548ae878b6c3528f47baf4ec518dbe7cd3b0109f
Taruna Emani <temani@andrew.cmu.edu>
2018-05-24 16:19:59 -0400

    heroku error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
-  include AppHelpers::Cart
   def new
   end
 
-  def create
-    create_cart
-  end
-
-  def destroy
-    destroy_cart
-  end
+  # def create
+  #   create_cart
+  # end
+  #
+  # def destroy
+  #   destroy_cart
+  # end
 end
