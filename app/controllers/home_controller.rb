fb8b912a0fb17ea7e87bc4241dfecfad4fc189b9
Taruna Emani <temani@andrew.cmu.edu>
2018-05-25 03:58:17 -0400

    finished withdrawal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,4 +13,19 @@ class HomeController < ApplicationController
   def cart
     @total = calculate_total_cart_cost
   end
+
+  def withdrawal
+    @total = calculate_total_cart_cost
+    num = "5b03908f322fa06b677938fb"
+    data = {
+        "medium": "balance",
+        "transaction_date": Date.today,
+        "status": "pending",
+        "amount": @total,
+        "description": "string"
+    }
+    Withdrawal.createWithdrawal(num, data)
+    clear_cart
+    return nil
+  end
 end
